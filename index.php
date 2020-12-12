0241fa131e8972f4eaa91e36c18f0ec9bc8700b8
renato <renatoluhe@gmail.com>
2020-12-12 01:58:36 -0500

    mi primer commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css.css">
+    <title>proyecto git</title>
+</head>
+<body>
+    
+    <h1>hola</h1>
+
+
+
+    <script src="js.js"></script>
+</body>
+</html>
